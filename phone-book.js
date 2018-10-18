b46ee21a37a5142630b1da905575985c14f1e684
Nikita Lebedev <acke208@yandex.ru>
2018-10-18 23:56:10 +0500

    исправления

--- phone-book.js
+++ phone-book.js
@@ -19,7 +19,7 @@ let phoneBook = new Set();
  * @returns {Boolean}
  */
 function add(phone, name, email) {
-    email = email.trim() || '';
+    email = email || '';
     if (!isDataCorrect(phone, name, email)) {
         return false;
     }
@@ -39,7 +39,7 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
-    email = email.trim() || '';
+    email = email || '';
     if (!isDataCorrect(phone, name, email)) {
         return false;
     }
