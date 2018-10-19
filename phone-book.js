2312354da544f0696b35f4948869c4fb09f1776f
Alex768 <vortigont768@inbox.ru>
2018-10-19 22:48:17 +0500

    initial

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,34 @@ const isStar = true;
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = new Map();
+
+/**
+ * Проверка корректности телефонного номера
+ * @param {String} phone
+ * @returns {Boolean}
+ */
+function isValidPhone(phone) {
+    if (typeof phone !== 'string') {
+        return false;
+    }
+    const PHONE_PATTERN = /^\d{10}$/;
+
+    return PHONE_PATTERN.test(phone);
+}
+
+/**
+ * Возвращает строкове представление имени
+ * @param {String} name
+ * @returns {Boolean}
+ */
+function isValidName(name) {
+    if (typeof name !== 'string') {
+        return false;
+    }
+
+    return name.length > 0;
+}
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +46,21 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    if (!isValidPhone(phone)) {
+        return false;
+    }
+    if (!isValidName(name)) {
+        return false;
+    }
+    if (phoneBook.has(phone)) {
+        return false;
+    }
+    phoneBook.set(phone, {
+        name: name,
+        email: (typeof email === 'string' ? email : '')
+    });
 
+    return true;
 }
 
 /**
@@ -30,7 +71,49 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    if (!isValidPhone(phone)) {
+        return false;
+    }
+    if (!phoneBook.has(phone)) {
+        return false;
+    }
+    let entry = phoneBook.get(phone);
+    if (isValidName(name)) {
+        entry.name = name;
+    }
+    entry.email = (typeof email === 'string' ? email : '');
+    phoneBook.set(phone, entry);
 
+    return true;
+}
+
+/**
+ * Возвращает массив объектов с контактными данными из справочника, соответствующих запросу
+ * @param {String} query - поисковый запрос
+ * @returns {Array}
+ */
+function findEntries(query) {
+    const result = [];
+    phoneBook.forEach((personalData, phone) => {
+        personalData.phone = phone;
+        if (query === '*') {
+            result.push(personalData);
+
+            return;
+        }
+        for (let key in personalData) {
+            if (!personalData.hasOwnProperty(key)) {
+                continue;
+            }
+            if (personalData[key].indexOf(query) !== -1) {
+                result.push(personalData);
+
+                return;
+            }
+        }
+    });
+
+    return result;
 }
 
 /**
@@ -39,7 +122,29 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    const entries = findEntries(query);
+    entries.forEach(entry => {
+        phoneBook.delete(entry.phone);
+    });
 
+    return entries.length;
+}
+
+/**
+ * Форматирует номер телефона
+ * @param {Object} personalData - данные о контакте: номер телефона, имя и адрес эл. почты
+ * @returns {String}
+ */
+function getTextRepresentation(personalData) {
+
+    function formatPhone(phone) {
+        return '+7 (' + phone.slice(0, 3) + ') ' + phone.slice(3, 3) + '-' +
+            phone.slice(6, 2) + '-' + phone.slice(8, 2);
+    }
+
+    return formatPhone(personalData.phone) +
+        (personalData.name.length ? ', ' + personalData.name : '') +
+        (personalData.email.length ? ', ' + personalData.email : '');
 }
 
 /**
@@ -48,7 +153,18 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    if (typeof query !== 'string') {
+        return [];
+    }
+    if (!query.length) {
+        return [];
+    }
+    const entries = findEntries(query);
+    entries.map(entry => {
+        return getTextRepresentation(entry);
+    });
 
+    return entries;
 }
 
 /**
@@ -58,11 +174,21 @@ function find(query) {
  * @returns {Number} – количество добавленных и обновленных записей
  */
 function importFromCsv(csv) {
-    // Парсим csv
-    // Добавляем в телефонную книгу
-    // Либо обновляем, если запись с таким телефоном уже существует
+    const NEW_LINE_PATTERN = /[\r\n]+/;
+    const FIELDS_DELIMITER = ';';
+    let successOperationsCount = 0;
+    csv.split(NEW_LINE_PATTERN).forEach(line => {
+        const values = line.split(FIELDS_DELIMITER);
+        const entry = {
+            'name': values[0],
+            'phone': values[1],
+            'email': values[2]
+        };
+        const method = !phoneBook.has(entry.phone) ? add : update;
+        successOperationsCount += method(entry.phone, entry.name, entry.email) ? 1 : 0;
+    });
 
-    return csv.split('\n').length;
+    return successOperationsCount;
 }
 
 module.exports = {
@@ -71,6 +197,10 @@ module.exports = {
     findAndRemove,
     find,
     importFromCsv,
+    isValidPhone,
+    isValidName,
+    findEntries,
+    getTextRepresentation,
 
     isStar
 };
