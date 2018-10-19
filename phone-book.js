9c446c5cecbfc0af0271ccdd3f2720c38eb2c6d9
Dm17r1y <dmitriy.kopylov2016@gmail.com>
2018-10-19 23:07:18 +0500

    Решение

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ const isStar = true;
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = {};
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +19,24 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    if (!phone.match(/^\d{10}$/)) {
+        return false;
+    }
 
+    if (phoneBook.hasOwnProperty(phone)) {
+        return false;
+    }
+
+    if (!name) {
+        return false;
+    }
+
+    phoneBook[phone] = {
+        name: name,
+        email: email
+    };
+
+    return true;
 }
 
 /**
@@ -30,7 +47,20 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    if (!phoneBook.hasOwnProperty(phone)) {
+        return false;
+    }
 
+    if (!name) {
+        return false;
+    }
+
+    phoneBook[phone] = {
+        name: name,
+        email: email
+    };
+
+    return true;
 }
 
 /**
@@ -39,7 +69,26 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    const phones = findPhones(query);
+    phones.forEach(phone => {
+        phoneBook[phone] = undefined;
+    });
 
+    return phones.length;
+}
+
+function findPhones(query) {
+    if (query === '*') {
+        return Object.keys(phoneBook);
+    }
+
+    if (query === '') {
+        return [];
+    }
+
+    return Object.keys(phoneBook)
+        .filter(key => key.includes(query) || phoneBook[key].name.includes(query) ||
+            (phoneBook[key].email !== undefined && phoneBook[key].email.includes(query)));
 }
 
 /**
@@ -48,7 +97,16 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    const transformPhone = phone => `+7 (${phone.substr(0, 3)}) ` +
+    `${phone.substr(3, 3)}-${phone.substr(6, 2)}-${phone.substr(8, 2)}`;
 
+    return findPhones(query)
+        .sort((firstPhone, secondPhone) =>
+            phoneBook[firstPhone].name.localeCompare(phoneBook[secondPhone].name))
+        .map(phone =>[phoneBook[phone].name, transformPhone(phone),
+            phoneBook[phone].email]
+            .filter(field => field !== undefined)
+            .join(', '));
 }
 
 /**
@@ -61,8 +119,16 @@ function importFromCsv(csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
+    let count = 0;
+    csv.split('\n')
+        .forEach(record => {
+            const arr = record.split(';');
+            if (add(arr[1], arr[0], arr[2]) || update(arr[1], arr[0], arr[2])) {
+                count++;
+            }
+        });
 
-    return csv.split('\n').length;
+    return count;
 }
 
 module.exports = {
