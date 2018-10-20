99e0ea610c5c390bc317abc5e04b4696b7715abf
Daria <DaArtPetrova@mail.ru>
2018-10-20 21:43:09 +0500

    First attemtpt

--- phone-book.js
+++ phone-book.js
@@ -10,6 +10,45 @@ const isStar = true;
  * Телефонная книга
  */
 let phoneBook;
+phoneBook = new Map();
+
+function checkPhone(phone) {
+    return /^\d{10}$/.test(phone);
+}
+
+function formatPhone(phone) {
+    return '+7 (' + phone.slice(0, 3) + ') ' + phone.slice(3, 6) + '-' +
+        phone.slice(6, 8) + '-' + phone.slice(8, 10);
+}
+
+function findMap(query) {
+    if (query === '*') {
+        return phoneBook;
+    }
+    const reg = new RegExp(query);
+    const found = new Map();
+    phoneBook.forEach(function (value, key) {
+        if (reg.test(key) || reg.test(value.name) || reg.test(value.email)) {
+            found.set(key, value);
+        }
+    });
+
+    return found;
+}
+
+function decorateFoundMap(foundMap) {
+    const result = [];
+    foundMap.forEach(function (value, key) {
+        const formattedPhone = formatPhone(key);
+        let note = value.name + ', ' + formattedPhone;
+        if (value.email !== '') {
+            note += ', ' + value.email;
+        }
+        result.push(note);
+    });
+
+    return result.sort();
+}
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +58,12 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    if ((!checkPhone(phone)) || (phoneBook.has(phone)) || (arguments.length === 1)) {
+        return false;
+    }
+    phoneBook.set(phone, { 'name': name, 'email': email });
 
+    return true;
 }
 
 /**
@@ -30,7 +74,15 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    if (arguments.length === 1 || !phoneBook.has(phone)) {
+        return false;
+    }
+    if (arguments.length === 2) {
+        email = '';
+    }
+    phoneBook.set(phone, { 'name': name, 'email': email });
 
+    return true;
 }
 
 /**
@@ -39,7 +91,12 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    const foundMap = findMap(query);
+    foundMap.forEach(function (value, key) {
+        phoneBook.delete(key);
+    });
 
+    return foundMap.size;
 }
 
 /**
@@ -48,7 +105,9 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    const foundMap = findMap(query);
 
+    return decorateFoundMap(foundMap);
 }
 
 /**
@@ -61,8 +120,19 @@ function importFromCsv(csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
+    let addedOrUpdatedCount = 0;
+    const csvStrings = csv.split('\n');
+    csvStrings.forEach(function (item) {
+        const parts = item.split(';');
+        const name = parts[0];
+        const phone = parts[1];
+        const email = parts[2];
+        if (add(phone, name, email) || update(phone, name, email)) {
+            addedOrUpdatedCount++;
+        }
+    });
 
-    return csv.split('\n').length;
+    return addedOrUpdatedCount;
 }
 
 module.exports = {
