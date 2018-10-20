c63d9656f9cbc4591aa469d18053efd5cc892d6d
hamy11 <mm.di@mail.ru>
2018-10-20 17:01:47 +0500

    hmm

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ const isStar = true;
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = {};
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +19,7 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
-
+    return tryAddRecord(phone, name, email);
 }
 
 /**
@@ -30,7 +30,7 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
-
+    return tryUpdateRecord(phone, name, email);
 }
 
 /**
@@ -39,7 +39,19 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    if (!query) {
+        return 0;
+    }
+    let removedCount = 0;
+    for (let [phone, { name, email }] of Object.entries(phoneBook)) {
+        let summary = [name, phone, email, '*'];
+        if (paramsOccurencyFound(summary, query)) {
+            delete phoneBook[phone];
+            removedCount++;
+        }
+    }
 
+    return removedCount;
 }
 
 /**
@@ -48,7 +60,20 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    if (!query) {
+        return [];
+    }
+    let searchResult = [];
+    for (let [phone, { name, email }] of Object.entries(phoneBook)) {
+        let summary = [name, phone, email, '*'];
+        if (paramsOccurencyFound(summary, query)) {
+            let formattedOutput = formatOutput(name, phone, email);
+            searchResult.push({ name, formattedOutput });
+        }
+    }
+    let sortedResult = searchResult.sort((a, b) => a.name > b.name).map(x => x.formattedOutput);
 
+    return sortedResult;
 }
 
 /**
@@ -62,7 +87,78 @@ function importFromCsv(csv) {
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
 
-    return csv.split('\n').length;
+    var records = csv.split('\n');
+    let newRecordsCount = 0;
+    let updatedRecordsCount = 0;
+    for (var i = 0; i < records.length; i++) {
+        let [name, phone, email] = records[i].split(';');
+        if (tryAddRecord(phone, name, email)) {
+            newRecordsCount++;
+        } else if (tryUpdateRecord(phone, name, email)) {
+            updatedRecordsCount++;
+        }
+    }
+
+    return newRecordsCount + updatedRecordsCount;
+}
+
+function tryAddRecord(phone, name, email) {
+    if (!name || !isValidPhone(phone) || phoneBook.hasOwnProperty(phone)) {
+        return false;
+    }
+    phoneBook[phone] = { name, email };
+
+    return true;
+}
+
+function tryUpdateRecord(phone, name, email) {
+    if (!phoneBook.hasOwnProperty(phone) || !name) {
+        return false;
+    }
+    phoneBook[phone] = { name, email };
+
+    return true;
+}
+
+/**
+ * Поиск вхождений запроса в запись
+ * @param {Array} summary
+ * @param {String} query
+ * @returns {Boolean}
+ */
+function paramsOccurencyFound(summary, query) {
+    for (let param of summary) {
+        if (param && param.indexOf(query) >= 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * Форматирование вывода
+ * @param {String} name
+ * @param {String} phone
+ * @param {String} email
+ * @returns {String}
+ */
+function formatOutput(name, phone, email) {
+    let baseOutput = `${name}, ${formatNumber(phone)}`;
+    let formattedOutput = email ? baseOutput + `, ${email}` : baseOutput;
+
+    return formattedOutput;
+}
+
+function isValidPhone(phone) {
+    return /^([0-9]){10}$/.test(phone);
+}
+
+function formatNumber(number) {
+    return `+7 (${number.slice(0, 3)}) ` +
+        `${number.slice(3, 6)}-` +
+        `${number.slice(6, 8)}-` +
+        `${number.slice(8)}`;
 }
 
 module.exports = {
