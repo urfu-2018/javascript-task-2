faf1b8035f747a7575a76fd1ae42f1db018ab18e
Shchupakov Antony <a.shchupakov@mail.ru>
2018-10-21 20:51:45 +0500

    Add first solution

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,32 @@ const isStar = true;
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = new Map();
+
+function isPhoneValid(phone) {
+    if (typeof phone !== 'undefined') {
+        return phone.match(/^\d{10}$/);
+    }
+
+    return false;
+}
+
+function getUsers(query) {
+    if (query === '') {
+        return [];
+    }
+    const users = [];
+    const isUniQuery = query === '*';
+    phoneBook.forEach((v, k) => {
+        const emailResult = typeof v.email === 'undefined' ? false : v.email.includes(query);
+
+        if (isUniQuery || v.name.includes(query) || emailResult || k.includes(query)) {
+            users.push({ name: v.name, phone: k, email: v.email });
+        }
+    });
+
+    return users;
+}
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +44,14 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    const hasName = typeof name !== 'undefined' && name !== '';
+    if (isPhoneValid(phone) && hasName && !phoneBook.has(phone)) {
+        phoneBook.set(phone, { name: name, email: email });
 
+        return true;
+    }
+
+    return false;
 }
 
 /**
@@ -30,7 +62,14 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    const person = phoneBook.get(phone);
+    if (typeof person === 'undefined') {
+        return false;
+    }
+    person.name = (typeof name === 'undefined') || (name === '') ? person.name : name;
+    person.email = email === '' ? undefined : email;
 
+    return true;
 }
 
 /**
@@ -39,7 +78,10 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    const users = getUsers(query);
+    users.forEach(e => phoneBook.delete(e.phone));
 
+    return users.length;
 }
 
 /**
@@ -48,7 +90,29 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    const users = getUsers(query);
 
+    users.sort(function (a, b) {
+        if (a.name > b.name) {
+            return 1;
+        }
+        if (a.name < b.name) {
+            return -1;
+        }
+
+        return 0;
+    });
+
+    return users.map(function (value) {
+        const p = value.phone;
+        const phone = `+7 (${p.slice(0, 3)}) ${p.slice(3, 6)}-${p.slice(6, 8)}-${p.slice(8)}`;
+        const result = `${value.name}, ${phone}`;
+        if (typeof value.email !== 'undefined') {
+            return `${result}, ${value.email}`;
+        }
+
+        return result;
+    });
 }
 
 /**
@@ -62,7 +126,20 @@ function importFromCsv(csv) {
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
 
-    return csv.split('\n').length;
+    const users = csv.split('\n');
+    let count = 0;
+    users.forEach(function (user) {
+        const args = user.split(';');
+        const name = args[0];
+        const phone = args[1];
+        const email = args[2];
+
+        if (add(phone, name, email) || update(phone, name, email)) {
+            count++;
+        }
+    });
+
+    return count;
 }
 
 module.exports = {
