1ff7db61188c007d46ef9d25505675192e04f583
cfgi <cfgi@yandex.ru>
2018-10-22 01:55:19 +0500

    Just test without extra task yet

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ const isStar = true;
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = new Map();
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +19,18 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    if (!checking(phone, name)) {
+        return false;
+    }
+    if (phoneBook.get(phone) === undefined) {
+        phoneBook.set(phone, [name, email]);
 
+        // console.info(phoneBook);
+
+        return true;
+    }
+
+    return false;
 }
 
 /**
@@ -30,7 +41,17 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    if (!checking(phone, name)) {
+        throw new TypeError();
+    }
+    if (phoneBook.get(phone) !== undefined) {
+        phoneBook.set(phone, [name, email]);
+        // console.info(phoneBook);
 
+        return true;
+    }
+
+    return false;
 }
 
 /**
@@ -39,7 +60,23 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    let res = 0;
+    let iterator = phoneBook.keys();
+    let size = phoneBook.size;
+    for (let i = 0; i < size; i++) {
+        let phone = iterator.next().value.toString();
+        let name = phoneBook.get(phone)[0].toString();
+        let email = '';
+        if (phoneBook.get(phone)[1] !== undefined) {
+            email = phoneBook.get(phone)[1].toString();
+        }
+        if (isIncludes([phone, name, email], query)) {
+            phoneBook.delete(phone);
+            res ++;
+        }
+    }
 
+    return res;
 }
 
 /**
@@ -48,7 +85,24 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    if (query === '*') {
+        query = '';
+    }
+    let res = [];
+    let iterator = phoneBook.keys();
+    for (let i = 0; i < phoneBook.size; i++) {
+        let phone = iterator.next().value.toString();
+        let name = phoneBook.get(phone)[0].toString();
+        let email = '';
+        if (phoneBook.get(phone)[1] !== undefined) {
+            email = phoneBook.get(phone)[1].toString();
+        }
+        if (isIncludes([phone, name, email], query)) {
+            res.push([phone, name, email]);
+        }
+    }
 
+    return res;
 }
 
 /**
@@ -63,6 +117,27 @@ function importFromCsv(csv) {
     // Либо обновляем, если запись с таким телефоном уже существует
 
     return csv.split('\n').length;
+}
+
+function isIncludes(strings, includingString) {
+    let res = false;
+    for (let i = 0; i < strings.length; i++) {
+        if (strings[i].includes(includingString)) {
+            res = true;
+        }
+        if (res) {
+            return res;
+        }
+    }
+
+    return false;
+}
+
+function checking(phone, name) {
+    const phoneRegexp = new RegExp('^[0-9]{10}$|^[0-9]{6}$');
+
+    return ((typeof phone && typeof name) === 'string' && name.length !== 0 &&
+        phoneRegexp.test(phone));
 }
 
 module.exports = {
