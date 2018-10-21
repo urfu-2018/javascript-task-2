5779e7e07d4b2d43bb3e438627d8f931b6d870a6
artem <artem.gafetinov@gmail.com>
2018-10-21 15:38:09 +0500

    complete

--- phone-book.js
+++ phone-book.js
@@ -104,10 +104,8 @@ function find(query) {
 
 function getNote(index) {
     let note;
-    if (typeof phoneBook[index].name !== 'undefined')
-    {
-        note = `${phoneBook[index].name}, ${getCorrectPhoneFormat(phoneBook[index].phone)}`;
-    }
+    if (typeof phoneBook[index].name !== 'undefined') {
+        note = `${phoneBook[index].name}, ${getCorrectPhoneFormat(phoneBook[index].phone)}`; }
     else {
         note = `${getCorrectPhoneFormat(phoneBook[index].phone)}`;
     }
