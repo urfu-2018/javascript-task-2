e44ffef71256c01ca4bedc5df502785f988d56a9
Victor <evdokimovvik@gmail.com>
2018-10-22 21:20:07 +0500

    phone book

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ const isStar = true;
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = {};
 
 /**
  * Добавление записи в телефонную книгу
@@ -18,8 +18,28 @@ let phoneBook;
  * @param {String?} email
  * @returns {Boolean}
  */
-function add(phone, name, email) {
 
+function add(phone, name, email) {
+    const regExpPhone = /^\d{10}$/;
+    name = getEmptyIfNotString(name);
+    email = getEmptyIfNotString(email);
+    if (typeof phone === 'string' &&
+        regExpPhone.test(phone) &&
+        phoneBook[phone] === undefined &&
+        name !== '') {
+        phoneBook[phone] = {
+            name: name,
+            email: email
+        };
+
+        return true;
+    }
+
+    return false;
+}
+
+function getEmptyIfNotString(str) {
+    return typeof str === 'string' ? str : '';
 }
 
 /**
@@ -30,7 +50,19 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    name = getEmptyIfNotString(name);
+    email = getEmptyIfNotString(email);
+    if (phoneBook[phone] !== undefined &&
+        name !== '') {
+        phoneBook[phone] = {
+            name: name,
+            email: email
+        };
 
+        return true;
+    }
+
+    return false;
 }
 
 /**
@@ -39,7 +71,24 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    if (typeof query !== 'string' || query === '') {
+        return;
+    }
+    let countDeleted = 0;
+    for (const phone in phoneBook) {
+        if (isFind(phone, query)) {
+            delete phoneBook[phone];
+            countDeleted++;
+        }
+    }
 
+    return countDeleted;
+}
+
+function isFind(phone, query) {
+    return phone.indexOf(query) !== -1 ||
+        phoneBook[phone].name.indexOf(query) !== -1 ||
+        phoneBook[phone].email.indexOf(query) !== -1;
 }
 
 /**
@@ -48,7 +97,57 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    const arrFindPhones = [];
+    if (typeof query !== 'string' && query === '') {
+        return;
+    }
+    for (const phone in phoneBook) {
+        if (query === '*') {
+            arrFindPhones.push([phoneBook[phone].name, phone, phoneBook[phone].email]);
+        } else if (isFind(phone, query)) {
+            arrFindPhones.push([phoneBook[phone].name, phone, phoneBook[phone].email]);
+        }
+    }
+    arrFindPhones.sort(sortedPhonesByName);
 
+    return getFormatString(arrFindPhones);
+}
+
+function sortedPhonesByName(manA, manB) {
+    if (manA[0].toLowerCase() > manB[0].toLowerCase()) {
+        return 1;
+    }
+    if (manA[0].toLowerCase() < manB[0].toLowerCase()) {
+        return -1;
+    }
+
+    return 0;
+}
+
+function getFormatString(contacts) {
+    const formatForPhone = /(\d{3})(\d{3})(\d{2})(\d{2})/;
+    const arrFormatContacts = [];
+    for (let contact of contacts) {
+        contact[1] = contact[1].replace(formatForPhone, replacer);
+        arrFormatContacts.push(getConcatContact(contact));
+    }
+
+    return arrFormatContacts;
+}
+
+function replacer() {
+    return '+7 (' + arguments[1] + ') ' + [arguments[2], arguments[3], arguments[4]].join('-');
+}
+
+function getConcatContact(contact) {
+    let contactsInStr = contact[0];
+    for (let i = 1; i < contact.length; i++) {
+        if (contact[i].length > 0) {
+            contactsInStr += ', ' + contact[i];
+        }
+    }
+
+    return contactsInStr;
 }
 
 /**
@@ -61,8 +160,22 @@ function importFromCsv(csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
+    const contactsInStr = csv.split('\n');
+    let countAdded = 0;
+    for (const contact of contactsInStr) {
+        const contactInArr = contact.split(';');
+        if (phoneBook[contactInArr[1]] === undefined) {
+            countAdded += isSuccess(add(contactInArr[1], contactInArr[0], contactInArr[2]));
+        } else {
+            countAdded += isSuccess(update(contactInArr[1], contactInArr[0], contactInArr[2]));
+        }
+    }
 
-    return csv.split('\n').length;
+    return countAdded;
+}
+
+function isSuccess(isAdd) {
+    return isAdd ? 1 : 0;
 }
 
 module.exports = {
