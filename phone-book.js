e4a7c521b82ecefc308f79dee325899f4c27ba9c
maxticorn <letunovskeymax@gmail.com>
2018-10-23 17:34:35 +0500

    done

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ const isStar = true;
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = [];
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +19,26 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    const isPhoneValid = /^\d{10}$/.test(phone);
+    if (!isPhoneValid || !name) {
+        return false;
+    }
+    const containsPhone = phoneBook.find(
+        (entry) => {
+            return entry.phone === phone;
+        }
+    );
+    if (containsPhone) {
+        return false;
+    }
 
+    phoneBook.push({
+        phone: phone,
+        name: name,
+        email: email
+    });
+
+    return true;
 }
 
 /**
@@ -30,7 +49,40 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    if (name === undefined || name.length === 0) {
+        return false;
+    }
 
+    const found = phoneBook.find(
+        (entry) => entry.phone === phone
+    );
+    if (found) {
+        found.name = name;
+        found.email = email;
+
+        return true;
+    }
+
+    return false;
+}
+
+function findEntries(query) {
+    if (query === undefined || query.length === 0) {
+        return [];
+    }
+
+    if (query === '*') {
+        return phoneBook.slice();
+    }
+    const result = phoneBook.filter(
+        (entry) => {
+            return entry.phone.includes(query) ||
+                entry.name.includes(query) ||
+                entry.email !== undefined && entry.email.includes(query);
+        }
+    );
+
+    return result;
 }
 
 /**
@@ -39,7 +91,27 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    const findResult = findEntries(query);
+    phoneBook = phoneBook.filter(
+        (entry) => !findResult.includes(entry)
+    );
 
+    return findResult.length;
+}
+
+function formatPhone(phone) {
+    return `+7 (${phone.substring(0, 3)}) ` +
+        `${phone.substring(3, 6)}-` +
+        `${phone.substring(6, 8)}-` +
+        `${phone.substring(8, 10)}`;
+}
+
+function formatContact(contact) {
+    if (contact.email) {
+        return `${contact.name}, ${formatPhone(contact.phone)}, ${contact.email}`;
+    }
+
+    return `${contact.name}, ${formatPhone(contact.phone)}`;
 }
 
 /**
@@ -48,7 +120,12 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    const result = findEntries(query);
+    result.sort((e1, e2) => {
+        return e1.name > e2.name;
+    });
 
+    return result.map(formatContact);
 }
 
 /**
@@ -61,8 +138,15 @@ function importFromCsv(csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
+    return csv.split('\n').map(i => {
+        const parts = i.split(';');
+        if (update(parts[1], parts[0], parts[2])) {
+            return true;
+        }
 
-    return csv.split('\n').length;
+        return add(parts[1], parts[0], parts[2]);
+    })
+        .filter(success => success).length;
 }
 
 module.exports = {
