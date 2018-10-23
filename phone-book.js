5dd14e3d305f4acb68eb107ceb805144865f973e
Yewert <blazer.andrew@gmail.com>
2018-10-23 20:30:13 +0500

    ......

--- phone-book.js
+++ phone-book.js
@@ -46,7 +46,7 @@ function update(phone, name, email) {
     if (!isString(phone) || !isString(name) || !/^\d{10}$/.test(phone)) {
         return false;
     }
-    if (!phoneBook.has(phone) || name.trim().length === 0) {
+    if (/* !phoneBook.has(phone) || */ name.trim().length === 0) {
         return false;
     }
 
