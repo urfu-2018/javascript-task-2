0205d1cc4c50627a37fed0030b6ef629a4d85061
Resinchen <alex1410alex@gmail.com>
2018-10-23 23:00:17 +0500

    Решение задачи

--- phone-book.js
+++ phone-book.js
@@ -4,12 +4,30 @@
  * Сделано задание на звездочку
  * Реализован метод importFromCsv
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = [];
+
+function checkFallPhone(phone) {
+    return phone === undefined || !/^\d\d\d\d\d\d\d\d\d\d$/.test(phone);
+}
+
+function checkFallEmail(email) {
+    return email !== undefined && !/\w+@\w+\.\w+/.test(email);
+}
+
+function checkContain(phoneNote) {
+    for (let i = 0; i < phoneBook.length; i++) {
+        if (phoneNote.mPhone === phoneBook[i].mPhone) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +37,11 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    if (checkFallPhone(phone) || name === undefined || checkFallEmail(email)){ return false;}
+    const phoneNote = {mPhone:phone, mName:name, mEmail:email};
+    if (checkContain(phoneNote)) {return false;}
 
+    return Boolean(phoneBook.push(phoneNote));
 }
 
 /**
@@ -29,8 +51,17 @@ function add(phone, name, email) {
  * @param {String?} email
  * @returns {Boolean}
  */
-function update(phone, name, email) {
 
+function update(phone, name, email) {
+    if (checkFallPhone(phone) || name === undefined || checkFallEmail(email)) { return false;}
+    if (!checkContain({mPhone:phone, mName:name, mEmail:email})) {return false;}
+    for (let i = 0; i < phoneBook.length; i++) {
+        if (phone === phoneBook[i].mPhone) {
+            phoneBook[i].mName = name;
+            phoneBook[i].mEmail = email;
+            return true;
+        }
+    }
 }
 
 /**
@@ -38,8 +69,33 @@ function update(phone, name, email) {
  * @param {String} query
  * @returns {Number}
  */
+/*
+* На вход принимает запрос в виде строки
+* Находит (смотри __find__) и удаляет все найденные записи
+* Возвращает число удаленных записей
+*/
 function findAndRemove(query) {
+    if (typeof(query) !== 'string') {return;}
+    let finded = find(query);
+    for (let i = 0; i < finded.length; i++) {
+        const splited = finded[i].split(', ');
+        const phoneNote;
+        if (splited.length === 2) {
+            phoneNote = {mPhone:splited[0], mName:splited[1], mEmail: undefined};
+        }
+        else {
+            phoneNote = {mPhone:splited[0], mName:splited[1], mEmail: splited[2]};
+        }
+        phoneBook.splice(phoneBook.indexOf(phoneNote), 1);
+    }
+    return finded.length;
+}
 
+function concatPhone(phone) {
+    return '+7 ('.concat(phone.slice(0, 3))
+    .concat(') ').concat(phone.slice(3, 6))
+    .concat('-').concat(phone.slice(6, 8))
+    .concat('-').concat(phone.slice(8, 10)); 
 }
 
 /**
@@ -48,7 +104,30 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
-
+    let result = [];
+    if (typeof(query) !== 'string') {return;}
+    if (query.length === 0) {return;}
+    if (query === '*') {
+        for (let i = 0; i < phoneBook.length; i++) {
+            let newPhone = concatPhone(phoneBook[i].mPhone);
+                result.push('' + phoneBook[i].mName + ', ' + newPhone + ', ' + phoneBook[i].mEmail)
+        }
+    }
+    for (let i = 0; i < phoneBook.length; i++) {
+        let newPhone = concatPhone(phoneBook[i].mPhone);
+        if (phoneBook[i].mPhone.indexOf(query) !== -1 || phoneBook[i].mName.indexOf(query) !== -1 || phoneBook[i].mEmail !== undefined && phoneBook[i].mEmail.indexOf(query) !== -1) {
+            result.push('' + phoneBook[i].mName + ', ' + newPhone + ', ' + phoneBook[i].mEmail)
+        }
+    }
+    return result.sort(function (a, b) {
+        if (a.mName > b.mName) {
+          return 1;
+        }
+        if (a.mName < b.mName) {
+          return -1;
+        }
+        return 0
+    });
 }
 
 /**
@@ -61,7 +140,7 @@ function importFromCsv(csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
-
+    
     return csv.split('\n').length;
 }
 
