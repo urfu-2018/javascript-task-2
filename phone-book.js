6bc17c0ef9a559847ad7291eb0265f45e2c503f2
Andrey Denisov <andenozr@mail.ru>
2018-10-24 22:48:08 +0500

    done work

--- phone-book.js
+++ phone-book.js
@@ -4,12 +4,12 @@
  * Сделано задание на звездочку
  * Реализован метод importFromCsv
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = [];
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +19,25 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    const regexp = /^\d{10}$/;
+    if (typeof phone === 'string' && regexp.test(phone) && name !== undefined &&
+    phoneBook[phone] === undefined) {
+        if (email) {
+            phoneBook[phone] = {
+                name: name,
+                email: email
+            };
+        } else {
+            phoneBook[phone] = {
+                name: name,
+                email: ''
+            };
+        }
 
+        return true;
+    }
+
+    return false;
 }
 
 /**
@@ -30,7 +48,25 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    const regexp = /^\d{10}$/;
+    if (typeof phone === 'string' && regexp.test(phone) && name !== undefined &&
+    phoneBook[phone] !== undefined) {
+        if (email) {
+            phoneBook[phone] = {
+                name: name,
+                email: email
+            };
+        } else {
+            phoneBook[phone] = {
+                name: name,
+                email: ''
+            };
+        }
 
+        return true;
+    }
+
+    return false;
 }
 
 /**
@@ -39,7 +75,29 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    if (typeof query !== 'string' || !query) {
+        return 0;
+    } else if (query === '*') {
+        const deletedQuantity = Object.keys(phoneBook).length;
+        phoneBook = [];
 
+        return deletedQuantity;
+    }
+
+    return deleteAndCount(query);
+}
+
+function deleteAndCount(query) {
+    let deletedQuantity = 0;
+    for (let phone in phoneBook) {
+        if (phone.includes(query) || phoneBook[phone].name.includes(query) ||
+        phoneBook[phone].email.includes(query)) {
+            delete phoneBook[phone];
+            deletedQuantity++;
+        }
+    }
+
+    return deletedQuantity;
 }
 
 /**
@@ -48,7 +106,47 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    let formatArray = [];
+    if (typeof(query) !== 'string' || query === '') {
+        return;
+    } else if (query === '*') {
+        for (let i = 0; i < Object.keys(phoneBook).length; i++) {
+            formatArray.push(rewriteInString(Object.keys(phoneBook)[i]));
+        }
+    } else {
+        formatArray = findMatching(query, formatArray);
+    }
 
+    return formatArray.sort(sortByName);
+}
+
+function rewriteInString(phone) {
+    let fixPhone = '+7 (' + phone.slice(0, 3) + ') ' + phone.slice(3, 6) +
+    '-' + phone.slice(6, 8) + '-' + phone.slice(8);
+
+    return (phoneBook[phone].name + ', ' + fixPhone + ', ' + phoneBook[phone].email);
+}
+
+function findMatching(query, formatArray) {
+    for (let i = 0; i < Object.keys(phoneBook).length; i++) {
+        if (Object.keys(phoneBook)[i].includes(query) ||
+        phoneBook[Object.keys(phoneBook)[i]].name.includes(query) ||
+        phoneBook[Object.keys(phoneBook)[i]].email.includes(query)) {
+            formatArray.push(rewriteInString(Object.keys(phoneBook)[i]));
+        }
+    }
+
+    return formatArray;
+}
+
+function sortByName(name1, name2) {
+    if (name1[0].toLowerCase() > name2[0].toLowerCase()) {
+        return 1;
+    } else if (name1[0].toLowerCase() < name2[0].toLowerCase()) {
+        return -1;
+    }
+
+    return 0;
 }
 
 /**
