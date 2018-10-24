322525f33c68f474511ec43340e505c7a7228484
JulieBlack666 <32227117+JulieBlack666@users.noreply.github.com>
2018-10-24 10:34:20 +0500

    first try

--- phone-book.js
+++ phone-book.js
@@ -4,12 +4,12 @@
  * Сделано задание на звездочку
  * Реализован метод importFromCsv
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = [];
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +19,13 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    if (!(/^\d{10}$/.test(phone)) || !name || phoneBook.some(x => x.phone === phone)) {
+        return false;
+    }
 
+    phoneBook.push({ name, phone, email });
+
+    return true;
 }
 
 /**
@@ -30,7 +36,15 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    if (!phoneBook.some(x => x.phone === phone) || phoneBook.some(e => e.name === name &&
+        e.phone === phone && e.email === email)) {
+        return false;
+    }
 
+    phoneBook = phoneBook.filter(e => e.phone !== phone);
+    phoneBook.push({ name, phone, email });
+
+    return true;
 }
 
 /**
@@ -39,7 +53,14 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    if (!query) {
+        return 0;
+    }
+    const lengthBeforeRemoval = phoneBook.length;
+    phoneBook = phoneBook.filter(e => !e.name.includes(query) && !e.phone.includes(query) &&
+        (typeof e.email === 'undefined' || !e.email.includes(query)));
 
+    return lengthBeforeRemoval - phoneBook.length;
 }
 
 /**
@@ -48,7 +69,21 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    if (!query) {
+        return [];
+    }
 
+    const resultNotes = query === '*' ? phoneBook
+        : phoneBook.filter(e => e.name.includes(query) || e.phone.includes(query) ||
+                        typeof e.email !== 'undefined' && e.email.includes(query));
+
+    return resultNotes.map(e => `${e.name}, ${formatPhoneNumber(e.phone)}` +
+            (typeof e.email !== 'undefined' ? `, ${e.email}` : '')).sort();
+}
+
+function formatPhoneNumber(phone) {
+    return `+7 (${phone.substring(0, 3)}) ${phone.substring(3, 6)}-${phone.substring(6, 8)}` +
+            `-${phone.substring(8, 10)}`;
 }
 
 /**
