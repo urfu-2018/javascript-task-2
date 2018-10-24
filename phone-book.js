ddaf9db54070d127ea42dfd2097c9d854d1dae5e
VitaliyDm <vitaldmitrie@gmail.com>
2018-10-24 19:44:33 +0500

    initial commit

--- phone-book.js
+++ phone-book.js
@@ -4,12 +4,31 @@
  * Сделано задание на звездочку
  * Реализован метод importFromCsv
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = new Map();
+
+
+const isParamsCorrect = (phone, name, email) => {
+    let isPhoneCorrect = /^\d{10}$/.test(phone);
+    let isNameFormatCorrect = name !== undefined;
+    let isEmailFormatCorrect =
+        /^[0-9a-z]+@[a-z0-9]+\.[a-z0-9]+$/i.test(email) || email === undefined;
+
+    return isPhoneCorrect && isNameFormatCorrect && isEmailFormatCorrect;
+};
+
+const convertAnswer = userInf => {
+    userInf = userInf.split(' ');
+    let phone = userInf[0];
+
+    return `${userInf[1]}, +7 (${
+        phone.slice(0, 3)}) ${phone.slice(3, 6)}-${phone.slice(6, 8)}-${phone.slice(8, 10)}` +
+        String(userInf[2] !== 'undefined' ? `, ${userInf[2]}` : '');
+};
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +38,13 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    if (!isParamsCorrect(phone, name, email) || phoneBook.has(phone)) {
+        return false;
+    }
 
+    phoneBook.set(phone, `${phone} ${name} ${email}`);
+
+    return true;
 }
 
 /**
@@ -30,7 +55,14 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    if (!phoneBook.has(phone) && !isParamsCorrect(phone, name, email)) {
+        return false;
+    }
 
+    phoneBook.set(phone,
+        `${phone} ${name} ${email}`);
+
+    return true;
 }
 
 /**
@@ -39,7 +71,22 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    if (query === '*') {
+        let phoneBookLen = phoneBook.length;
+        phoneBook = new Map();
 
+        return phoneBookLen;
+    }
+    let contactsToDelete = [];
+    let queryRegEx = new RegExp(query);
+    phoneBook.forEach((value, key) => {
+        if (queryRegEx.test(value)) {
+            contactsToDelete.push(key);
+        }
+    });
+    contactsToDelete.forEach(item => phoneBook.delete(item));
+
+    return contactsToDelete.length;
 }
 
 /**
@@ -48,7 +95,19 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    let contacts = [];
+    if (query === '*') {
+        phoneBook.forEach((value) => contacts.push(convertAnswer(value)));
+    } else {
+        let queryRegEx = new RegExp(query);
+        phoneBook.forEach((value) => {
+            if (queryRegEx.test(value)) {
+                contacts.push(convertAnswer(value));
+            }
+        });
+    }
 
+    return contacts.sort();
 }
 
 /**
