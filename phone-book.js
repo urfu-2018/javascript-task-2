666e968b06442ac33b472eab5eb36e2410a6515e
AndrewGomzyakov <32879688+AndrewGomzyakov@users.noreply.github.com>
2018-10-24 17:20:30 +0500

    Update phone-book.js

--- phone-book.js
+++ phone-book.js
@@ -116,6 +116,7 @@ function find(query) {
         for (let i = 0; i < phoneBook.length; i++) {
             rez.add(formatData(phoneBook[i]));
         }
+
         return rez.sort();
     }
     for (let i = 0; i < phoneBook.length; i++) {
