bb310f7501ad3b7713b2258a262609d6203f632a
Anton Vlasov <anton.vlasov@jetstyle.ru>
2018-10-25 02:49:09 +0500

    Не проходит 555-тест по непонятной причине

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ const isStar = true;
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = [];
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +19,31 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    // const phoneCheck = (acc) => {
+    //     const phonePattern1 = '1{3}|2{3}|3{3}|4{3}|5{3}|6{3}|7{3}|8{3}|9{3}|0{3}';
+    //     const phonePattern2 = '1{2}|2{2}|3{2}|4{2}|5{2}|6{2}|7{2}|8{2}|9{2}|0{2}';
+    //     const phoneRegexp = new RegExp(
+    //         '^(' + phonePattern1 + '){2}(' +
+    //         phonePattern2 + '){2}$'
+    //     );
 
+    //     return phoneRegexp.test(acc);
+    // };
+    const phoneCheck = /^(\d{3}){2}(\d{2}){2}$/.test(phone);
+    const nameCheck = typeof name !== 'undefined';
+    let nameInPhoneBookCheck = phoneBook.filter(acc => acc.phone === phone);
+
+    if (phoneCheck && nameCheck && nameInPhoneBookCheck.length === 0) {
+        phoneBook.push({
+            phone: phone,
+            name: name,
+            email: email
+        });
+
+        return true;
+    }
+
+    return false;
 }
 
 /**
@@ -30,7 +54,26 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    let phoneLogIndex = -1;
+    for (let i = 0; i < phoneBook.length; i++) {
+        if (phoneBook[i].phone === phone) {
+            phoneLogIndex = i;
+            break;
+        }
+    }
 
+    if (name && phoneLogIndex !== -1) {
+        phoneBook[phoneLogIndex].name = name;
+        if (!email) {
+            delete phoneBook[phoneLogIndex].email;
+        } else {
+            phoneBook[phoneLogIndex].email = email;
+        }
+
+        return true;
+    }
+
+    return false;
 }
 
 /**
@@ -39,7 +82,33 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    if (query === '*') {
+        const phoneBookLength = phoneBook.length;
+        phoneBook.length = 0;
 
+        return phoneBookLength;
+    }
+
+    if (!query) {
+        return 0;
+    }
+
+    let phoneBookQuerySet = phoneBook.map((acc, index) => {
+        const phoneCheck = acc.phone.includes(query);
+        const nameCheck = acc.name.includes(query);
+        if (!acc.email) {
+            return phoneCheck && nameCheck;
+        }
+        const emailCheck = acc.email.includes(query);
+
+        return [phoneCheck || nameCheck || emailCheck, index];
+    });
+    let phoneBookIndexList = phoneBookQuerySet.filter((acc) => (acc[0]))
+        .map((acc) => (acc[1]));
+
+    phoneBookIndexList.forEach((index) => (phoneBook.splice(index, 1)));
+
+    return phoneBookIndexList.length;
 }
 
 /**
@@ -48,7 +117,43 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    const func = (acc) => {
+        const phone = '+7 (' +
+            acc.phone.substring(0, 3) + ') ' +
+            acc.phone.substring(3, 6) + '-' +
+            acc.phone.substring(6, 8) + '-' +
+            acc.phone.substring(8, 10);
+        if (!acc.email) {
+            return [acc.name, phone].join(', ');
+        }
 
+        return [acc.name, phone, acc.email].join(', ');
+    };
+
+    if (!query) {
+        return [];
+    }
+
+    if (query === '*') {
+        return phoneBook
+            .sort((logOne, logTwo) => (logOne.name.localeCompare(logTwo.name)))
+            .map(func);
+    }
+
+    let phoneBookQuerySet = phoneBook.filter((acc) => {
+        const phoneCheck = acc.phone.includes(query);
+        const nameCheck = acc.name.includes(query);
+        if (!acc.email) {
+            return phoneCheck && nameCheck;
+        }
+        const emailCheck = acc.email.includes(query);
+
+        return phoneCheck || nameCheck || emailCheck;
+    });
+
+    return phoneBookQuerySet
+        .sort((logOne, logTwo) => (logOne.name.localeCompare(logTwo.name)))
+        .map(func);
 }
 
 /**
@@ -58,11 +163,17 @@ function find(query) {
  * @returns {Number} – количество добавленных и обновленных записей
  */
 function importFromCsv(csv) {
-    // Парсим csv
-    // Добавляем в телефонную книгу
-    // Либо обновляем, если запись с таким телефоном уже существует
+    const listOfPhoneLogs = csv.split('\n');
+    const result = listOfPhoneLogs.filter((log) => {
+        const logData = log.split(';');
+        const name = logData[0];
+        const phone = logData[1];
+        const email = logData[2];
 
-    return csv.split('\n').length;
+        return add(phone, name, email) || update(phone, name, email);
+    });
+
+    return result.length;
 }
 
 module.exports = {
