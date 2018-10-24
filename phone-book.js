9220c6c24e0ec3ece88e641e027fa1d0b2212ea9
Orilow <ilyaorlov1998@gmail.com>
2018-10-24 17:14:20 +0500

    don't working "add"

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,47 @@ const isStar = true;
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = new Set();
+
+
+const phoneReg = /^\d{10}$/;
+const isString = (arg) => typeof(arg) === 'string';
+function replaceBadTypes(phone, name, email) {
+    // arguments.map((arg) => arg || '').map((arg) => arg.trim());
+    // почему это не работает?(в гугле не нашел подходящего объяснения)
+    phone = phone || '';
+    name = name || '';
+    email = email || '';
+
+    return [phone.trim(), name.trim(), email.trim()];
+}
+const argsTypesAreCorrect = (phone, name, email) =>
+    (!isString(phone) || !isString(name) || !isString(email));
+const phoneIsSuitable = (phone) => phoneReg.test(phone);
+
+function telInPhoneBook(phone) {
+    for (let element of phoneBook) {
+        if (element.slice(0, 10) === phone) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function areArgsCorrect(phone, name, email) {
+    if (!argsTypesAreCorrect(phone, name, email)) {
+        return false;
+    }
+    if (!name) {
+        return false;
+    }
+    if (!phoneIsSuitable(phone)) {
+        return false;
+    }
+
+    return true;
+}
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,58 +59,101 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    let result = replaceBadTypes(phone, name, email);
+    phone = result[0];
+    name = result[1];
+    email = result[2];
+    if (!areArgsCorrect(phone, name, email)) {
+        return false;
+    }
+    if (telInPhoneBook(phone)) {
+        return false;
+    }
+    phoneBook.add(`${phone}|${name}|${email}`);
 
+    return true;
 }
 
-/**
- * Обновление записи в телефонной книге
- * @param {String} phone
- * @param {String?} name
- * @param {String?} email
- * @returns {Boolean}
- */
-function update(phone, name, email) {
+// /**
+//  * Обновление записи в телефонной книге
+//  * @param {String} phone
+//  * @param {String?} name
+//  * @param {String?} email
+//  * @returns {Boolean}
+//  */
+// function update(phone, name, email) {
+//     let result = replaceBadTypes(phone, name, email);
+//     phone = result[0];
+//     name = result[1];
+//     email = result[2];
+//     if (!areArgsCorrect(phone, name, email)) {
+//         return false;
+//     }
+//     if (!telInPhoneBook(phone)) {
+//         return false;
+//     }
+//     updatePhoneBook(phone, name, email);
 
-}
+//     return true;
+// }
 
-/**
- * Удаление записей по запросу из телефонной книги
- * @param {String} query
- * @returns {Number}
- */
-function findAndRemove(query) {
+// function updatePhoneBook(phone, name, email) {
+//     for (let element of phoneBook) {
+//         if (element.slice(0, 10) === phone) {
+//             phoneBook.delete(element);
+//             phoneBook.add(`${phone}|${name}|${email}`);
+//             break;
+//         }
+//     }
+// }
 
-}
+// /**
+//  * Удаление записей по запросу из телефонной книги
+//  * @param {String} query
+//  * @returns {Number}
+//  */
+// function findAndRemove(query) {
+//     query = 1;
 
-/**
- * Поиск записей по запросу в телефонной книге
- * @param {String} query
- * @returns {String[]}
- */
-function find(query) {
+//     return 0; //stub
+// }
 
-}
+// /**
+//  * Поиск записей по запросу в телефонной книге
+//  * @param {String} query
+//  * @returns {String[]}
+//  */
+// function find(query) {
+//     query = 1;
 
-/**
- * Импорт записей из csv-формата
- * @star
- * @param {String} csv
- * @returns {Number} – количество добавленных и обновленных записей
- */
-function importFromCsv(csv) {
-    // Парсим csv
-    // Добавляем в телефонную книгу
-    // Либо обновляем, если запись с таким телефоном уже существует
+//     return []; //stub
+// }
 
-    return csv.split('\n').length;
-}
+// /**
+//  * Импорт записей из csv-формата
+//  * @star
+//  * @param {String} csv
+//  * @returns {Number} – количество добавленных и обновленных записей
+//  */
+// function importFromCsv(csv) {
+//     // Парсим csv
+//     // Добавляем в телефонную книгу
+//     // Либо обновляем, если запись с таким телефоном уже существует
+
+//     return csv.split('\n').length;
+// }
+// add('5554440044', 'Григорий', 'grisha@example.com');
+// add('5552220022', 'Борис', 'boris@example.com');
+// add('5551110011', 'Алекс');
+// add('5551110011', 'Алекс');
+// update('5551110011', 'Алексей', 'alex@example.com');
 
 module.exports = {
     add,
-    update,
-    findAndRemove,
-    find,
-    importFromCsv,
+    // update,
+    // findAndRemove,
+    // find,
+    // importFromCsv,
 
     isStar
 };
