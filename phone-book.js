82479ad53c5d09165e9206bf7ea9e3427e9a9428
Veronica <vrindavana.vera@mail.ru>
2018-10-25 17:25:45 +0500

    пока не все

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,21 @@ const isStar = true;
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = {};
+
+/**
+ * Проверка параметров
+ * @param {String} phone
+ * @param {String?} name
+ * @param {String?} email
+ * @returns {Boolean}
+ */
+function trueParam(phone, name, email) {
+    var tel = /(^[0-9]{10}$)/;
+
+    return (typeof phone === 'string') && (tel.test(phone)) && (name !== '') &&
+        (email === undefined || typeof email === 'string');
+}
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +33,12 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    if (find(phone).length > 0 || (!trueParam(phone, name, email))) {
+        return false;
+    }
+    phoneBook[phone] = { name: name, email: email };
 
+    return true;
 }
 
 /**
@@ -30,7 +49,13 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    if (find(phone).length > 0 && (trueParam(phone, name, email))) {
+        phoneBook[phone] = { name: name, email: email };
 
+        return true;
+    }
+
+    return false;
 }
 
 /**
@@ -39,7 +64,16 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    if (query === '') {
+        return 0;
+    }
+    var count = 0;
+    for (var [key] of Object.entries(phoneBook)) {
+        delete phoneBook[key];
+        count++;
+    }
 
+    return count;
 }
 
 /**
@@ -48,7 +82,29 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    if (query === '') {
+        return [];
+    }
+    if (query === '*') {
+        query = '';
+    }
+    const result = [];
+    var res = '';
+    for (var [key, value] of Object.entries(phoneBook)) {
+        var newPhone = newNumber(key);
+        res = value.name + ',' + newPhone;
+        if (value.email !== undefined) {
+            res += ',' + value.email;
+        }
+    }
+    result.push(res);
 
+    return result.sort();
+}
+
+function newNumber(phone) {
+
+    return phone.replace(/^(\d{3})(\d{3})(\d{2})(\d{2})$/, '+7 ($1) $2-$3-$4');
 }
 
 /**
@@ -61,8 +117,19 @@ function importFromCsv(csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
+    if (csv === undefined || typeof (csv) !== 'string' || csv === '') {
+        return 0;
+    }
+    var count = 0;
+    var text = csv.split(';');
+    var name = text[0];
+    var phone = text[1];
+    var email = text[2];
+    if (add(phone, name, email) || update(phone, name, email)) {
+        count++;
+    }
 
-    return csv.split('\n').length;
+    return count;
 }
 
 module.exports = {
