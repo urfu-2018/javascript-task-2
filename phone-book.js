00a5e9aedc7ac3a76096463f71d4ab22141ab59b
Egor Naumov <Egor_Nmv@bk.ru>
2018-10-25 11:09:17 +0500

    Base+

--- phone-book.js
+++ phone-book.js
@@ -4,12 +4,12 @@
  * Сделано задание на звездочку
  * Реализован метод importFromCsv
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Телефонная книга
  */
-let phoneBook;
+let phoneBook = {};
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +19,14 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    if (typeof(phone) !== 'string' || !((/^\d{10}$/).test(phone)) || name === '' ||
+    name === undefined || phoneBook.hasOwnProperty(phone)) {
 
+        return false;
+    }
+    phoneBook[phone] = { name, email };
+
+    return true;
 }
 
 /**
@@ -30,7 +37,13 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    if (!(phone in phoneBook) || name === '' || name === undefined) {
 
+        return false;
+    }
+    phoneBook[phone] = { name, email };
+
+    return true;
 }
 
 /**
@@ -39,7 +52,12 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    const arrforDelete = findWithoutOutput(query);
+    for (let i of arrforDelete) {
+        delete phoneBook[i];
+    }
 
+    return arrforDelete.length;
 }
 
 /**
@@ -48,7 +66,51 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    if (typeof query !== 'string' || query === '' || query === undefined) {
 
+        return [];
+    }
+    if (query === '*') {
+        let arrofAll = sortArrayfromAtoZbyName(Object.keys(phoneBook));
+
+        return outPutofFind(arrofAll);
+    }
+
+    return outPutofFind(findWithoutOutput(query));
+}
+
+function outPutofFind(somearray) {
+    return somearray.map(phone => (phoneBook[phone].email)
+        ? [phoneBook[phone].name, transferPhone(phone), phoneBook[phone].email].join(', ')
+        : [phoneBook[phone].name, transferPhone(phone)].join(', '));
+}
+
+function findWithoutOutput(query) {
+    let allkey = Object.keys(phoneBook);
+    let detectedAr = choicebyPhone(allkey, query);
+
+    return sortArrayfromAtoZbyName(detectedAr);
+}
+
+function transferPhone(phone) {
+    const first3num = phone.substring(0, 3);
+    const second3num = phone.substring(3, 6);
+    const third2num = phone.substring(6, 8);
+    const fourth2num = phone.substring(8, 10);
+
+    return `+7 (${first3num}) ${second3num}-${third2num}-${fourth2num}`;
+}
+
+function choicebyPhone(somearray, query) {
+
+    return somearray.filter(phone => phoneBook[phone].name.includes(query) ||
+    (phoneBook[phone].email !== undefined && phoneBook[phone].email.includes(query)) ||
+    phone.includes(query));
+}
+
+function sortArrayfromAtoZbyName(somearray) {
+
+    return somearray.sort((a, b) => phoneBook[a].name > phoneBook[b].name);
 }
 
 /**
