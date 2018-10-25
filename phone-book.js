e23ee49aa7d65ac165d0a35b6baca515f022112b
MargaritaNik <32447196+MargaritaNik@users.noreply.github.com>
2018-10-25 11:01:13 +0500

    исправлена ошибкаб когда удалялось все, если подавался пробел

--- phone-book.js
+++ phone-book.js
@@ -89,7 +89,7 @@ function find(query) {
     let arrayWithDate = phoneBookToArray();
     if (query === '*') {
         return arrayWithDate.sort((a, b) => a[0].localeCompare(b[0]));
-    } else if (!query) {
+    } else if (!query || query === ' ') {
 
         return [];
     }
