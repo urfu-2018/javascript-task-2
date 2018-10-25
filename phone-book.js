150e14c1d396226e6ffa140553e0bbc383e1ce71
agrossers <33430529+agrossers@users.noreply.github.com>
2018-10-25 20:27:09 +0300

    Первый вариант решения

--- phone-book.js
+++ phone-book.js
@@ -19,7 +19,29 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
-
+    if (phone.match(/^\d+$/) !== null
+        && phone.length === 10
+        && name !== undefined) {
+        if (phoneBook === undefined) { 
+            phoneBook = new Map(); 
+        }
+        for(let key in phoneBook.keys()) {
+            if ((phoneBook[key].username === name
+                && key === phone
+                && phoneBook[key].useremail === email) || key === phone) {
+                return false;
+            }
+        }
+        let user = {
+            username : name,
+            useremail: email
+        };
+        phoneBook.set(phone, user);
+        //console.log(phoneBook.has(phone));
+        //console.log('==================');
+        return true
+    }
+    return false
 }
 
 /**
@@ -30,7 +52,17 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
-
+    if (phoneBook.has(phone)) {
+        if (name !== undefined) {
+            let user = {
+                username : name,
+                useremail: email
+            };
+            phoneBook.set(phone, user);
+        }
+        //console.log(phoneBook);
+    }
+    
 }
 
 /**
@@ -39,7 +71,13 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
-
+    let findres = find(query)
+    let val;
+    for (val of findres) {
+        let key = val[1].replace(/(\d{3})\) (\d{3})-(\d{2})-(\d{2})/, '$1$2$3$4').substring(4);
+        phoneBook.delete(key);
+    }
+    return findres.length;
 }
 
 /**
@@ -48,7 +86,19 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
-
+    if (query === '') {
+        return undefined
+    }
+    let patt = query
+    if (query === '*') {
+        patt = ''
+    }
+    let sorted = [...phoneBook.entries()].sort((a,b) => a[1].username > b[1].username).map(function(val) {
+        return [val[1].username, 
+                val[0].replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, '+7 ($1) $2-$3-$4'), 
+                val[1].useremail].filter(x => x !== undefined);
+    });
+    return sorted.filter(x => x.join('').includes(patt));
 }
 
 /**
@@ -61,8 +111,19 @@ function importFromCsv(csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
-
-    return csv.split('\n').length;
+    let users = csv.split('\n').map(function(val) {
+        let arr = val.split(';');
+        return [arr[1], arr[0], arr[2]];
+    });
+    let val;
+    let counter = 0;
+    for (val of users) {
+        if (add(...val)) {
+            counter++;
+        }
+    }
+    console.log(counter);
+    return counter;
 }
 
 module.exports = {
