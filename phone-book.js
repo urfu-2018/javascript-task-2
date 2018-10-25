024245f979e3ab856245f3b7e6b14acb2208332e
Semyon <boyarnikov@skbkontur.ru>
2018-10-25 11:51:40 +0500

    Немного рефакторинга

--- phone-book.js
+++ phone-book.js
@@ -11,19 +11,12 @@ const isStar = true;
  */
 let phoneBook = [];
 
-/**
- * Добавление записи в телефонную книгу
- * @param {String} phone
- * @param {String?} name
- * @param {String?} email
- * @returns {Boolean}
- */
-function phoneValidation(phone) {
-    return isString(phone) && /^\d{10}$/.test(phone);
-}
-
 function isString(parameter) {
     return typeof parameter === 'string';
+}
+
+function phoneValidation(phone) {
+    return isString(phone) && /^\d{10}$/.test(phone);
 }
 
 function isCorrect(parameter) {
@@ -34,14 +27,33 @@ function checkDoubles(personPhone) {
     return phoneBook.findIndex(({ phone }) => phone === personPhone);
 }
 
+function isInclude(item, query) {
+    const { name, phone, email } = item;
+
+    return name.includes(query) || phone.includes(query) || (email ? email.includes(query) : false);
+}
+
+function transformItem(item) {
+    const { phone, name, email } = item;
+
+    return (
+        `${name}, ` +
+        `+7 (${phone.slice(0, 3)}) ${phone.slice(3, 6)}-` +
+        `${phone.slice(6, 8)}-${phone.slice(8, 10)}` +
+        `${email ? ', ' + email : ''}`
+    );
+}
+
+/**
+ * Добавление записи в телефонную книгу
+ * @param {String} phone
+ * @param {String?} name
+ * @param {String?} email
+ * @returns {Boolean}
+ */
 function add(phone, name, email) {
     if (phoneValidation(phone) && checkDoubles(phone) < 0 && isCorrect(name)) {
-        const person = { phone, name };
-
-        if (isCorrect(email)) {
-            person.email = email;
-        }
-        phoneBook.push(person);
+        phoneBook.push({ phone, name, email });
 
         return true;
     }
@@ -60,12 +72,7 @@ function update(phone, name, email) {
     const index = checkDoubles(phone);
 
     if (index !== -1 && isCorrect(name)) {
-        const person = { phone, name };
-
-        if (isCorrect(email)) {
-            person.email = email;
-        }
-        phoneBook[index] = person;
+        phoneBook[index] = { phone, name, email };
 
         return true;
     }
@@ -87,30 +94,12 @@ function findAndRemove(query) {
         return initialLength;
     }
     if (isCorrect(query)) {
-        phoneBook = phoneBook.filter(
-            ({ name, phone, email }) =>
-                !(
-                    name.includes(query) ||
-                    phone.includes(query) ||
-                    (email ? email.includes(query) : false)
-                )
-        );
+        phoneBook = phoneBook.filter(person => !isInclude(person, query));
 
         return initialLength - phoneBook.length;
     }
 
     return 0;
-}
-
-function transformItem(item) {
-    const { phone, name, email } = item;
-
-    return (
-        `${name}, ` +
-        `+7 (${phone.slice(0, 3)}) ${phone.slice(3, 6)}-` +
-        `${phone.slice(6, 8)}-${phone.slice(8, 10)}` +
-        `${email ? ', ' + email : ''}`
-    );
 }
 
 /**
@@ -119,7 +108,7 @@ function transformItem(item) {
  * @returns {String[]}
  */
 function find(query) {
-    const clonePhoneBook = phoneBook.slice(0);
+    const clonePhoneBook = phoneBook.slice();
 
     if (query === '*') {
         return clonePhoneBook
@@ -128,12 +117,7 @@ function find(query) {
     }
     if (isCorrect(query)) {
         return clonePhoneBook
-            .filter(
-                ({ name, phone, email }) =>
-                    name.includes(query) ||
-                    phone.includes(query) ||
-                    (email ? email.includes(query) : false)
-            )
+            .filter(person => isInclude(person, query))
             .sort(({ name }, { name: _name }) => name.localeCompare(_name))
             .map(person => transformItem(person));
     }
@@ -155,7 +139,7 @@ function importFromCsv(csv) {
 
             return add(phone, name, email) || update(phone, name, email);
         })
-        .filter(x => x).length;
+        .filter(succsessAction => succsessAction).length;
 }
 
 module.exports = {
