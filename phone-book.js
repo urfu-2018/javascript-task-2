413c0537522b3a5343e69acfd8f29414bad70425
Монин Константин <33116124+Kuvadorian@users.noreply.github.com>
2018-11-03 15:13:27 +0500

    Отформатировал

--- phone-book.js
+++ phone-book.js
@@ -16,11 +16,7 @@ function isString(field) {
 }
 
 function isCorrectedPhone(phone) {
-    if (isString(phone) && /^[0-9]{10}$/.test(phone)) {
-        return true;
-    }
-
-    return false;
+    return isString(phone) && /^[0-9]{10}$/.test(phone);
 }
 
 function isCorrectedName(name) {
@@ -34,12 +30,12 @@ function isUndefined(field) {
 // Собираем нужный формат номера из полного номера
 function getPhone(fullPhone) {
     return fullPhone.substring(4, 7) + fullPhone.substring(9, 12) +
-    fullPhone.substring(13, 15) + fullPhone.substring(16, 19);
+        fullPhone.substring(13, 15) + fullPhone.substring(16, 19);
 }
 
 function getNeedFormat(records, phone) {
     return records[phone].name + ', +7 (' + phone.substring(0, 3) + ') ' + phone.substring(3, 6) +
-    '-' + phone.substring(6, 8) + '-' + phone.substring(8, 10);
+        '-' + phone.substring(6, 8) + '-' + phone.substring(8, 10);
 }
 
 /**
@@ -50,7 +46,15 @@ function getNeedFormat(records, phone) {
  * @returns {Boolean}
  */
 
-let incorrectedData = (name, phone) => {
+function addRecord(phone, name, email) {
+    email = email || '';
+    phoneBook[phone] = {
+        name: name,
+        email: email
+    };
+}
+
+const incorrectedData = (name, phone) => {
     return !isCorrectedName(name) || !isCorrectedPhone(phone) || isUndefined(name);
 };
 
@@ -58,11 +62,7 @@ function add(phone, name, email) {
     if (incorrectedData(name, phone) || !isUndefined(phoneBook[phone])) {
         return false;
     }
-    email = email || '';
-    phoneBook[phone] = {
-        name: name,
-        email: email
-    };
+    addRecord(phone, name, email);
 
     return true;
 }
@@ -78,11 +78,7 @@ function update(phone, name, email) {
     if (incorrectedData(name, phone) || isUndefined(phoneBook[phone])) {
         return false;
     }
-    email = email || '';
-    phoneBook[phone] = {
-        name: name,
-        email: email
-    };
+    addRecord(phone, name, email);
 
     return true;
 }
@@ -93,14 +89,15 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
-    let removable = find(query);
-    for (let record of removable) {
+    const dataToDelete = find(query);
+
+    for (let record of dataToDelete) {
         let str = record.split(', ');
         let phone = getPhone(str[1]);
         delete phoneBook[phone];
     }
 
-    return removable.length;
+    return dataToDelete.length;
 }
 
 /**
@@ -113,10 +110,13 @@ function find(query) {
     if (query === '') {
         return [];
     }
+
     if (query === '*') {
         return getFormattedRecors(phoneBook);
     }
+
     let bookRecords = [];
+
     bookRecords = getRecordsByQuery(query);
 
     return getFormattedRecors(bookRecords);
@@ -124,8 +124,10 @@ function find(query) {
 
 //  Проверка на существование записей в книге
 function getRecordsByQuery(query) {
-    let result = [];
+    const result = [];
+
     for (let phone of Object.keys(phoneBook)) {
+
         if (phone.indexOf(query) !== -1 || phoneBook[phone].name.indexOf(query) !== -1 ||
             phoneBook[phone].email.indexOf(query) !== -1) {
             result[phone] = {
@@ -139,16 +141,20 @@ function getRecordsByQuery(query) {
 }
 //  Получение форматированных записей из книги
 function getFormattedRecors(records) {
-    let result = [];
+    const result = [];
+
     for (let phone of Object.keys(records)) {
         let record = getNeedFormat(records, phone);
+
         if (records[phone].email.length !== 0) {
             record = record + ', ' + records[phone].email;
         }
+
         if (record.length !== 0) {
             result.push(record);
         }
     }
+
     result.sort();
 
     return result;
@@ -162,13 +168,17 @@ function getFormattedRecors(records) {
  */
 function importFromCsv(csv) {
     let count = 0;
-    let strs = csv.split('\n');
-    strs.forEach(item => {
-        let str = item.split(';');
-        let phone = str[1];
-        let name = str[0];
-        let email = str[2];
+    const records = csv.split('\n');
+
+    records.forEach(str => {
+        const items = str.split(';');
+        let phone;
+        let name;
+        let email;
+
+        [name, phone, email] = items;
         email = email || '';
+
         if (!isUndefined(phoneBook[phone])) {
             if (update(phone, name, email)) {
                 count++;
