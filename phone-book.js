6b4055425a33ccdd138e813aae03fda8f36d362b
Maxim <ya.max-m2013@yandex.ru>
2018-11-19 19:11:35 +0500

    firstTry

--- phone-book.js
+++ phone-book.js
@@ -4,12 +4,12 @@
  * Сделано задание на звездочку
  * Реализован метод importFromCsv
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Телефонная книга
  */
-let phoneBook;
+var phoneBook = new Map();
 
 /**
  * Добавление записи в телефонную книгу
@@ -19,7 +19,24 @@ let phoneBook;
  * @returns {Boolean}
  */
 function add(phone, name, email) {
+    if (typeof(email) === 'undefined') {
+        email = '';
+    }
+    if (areCorrectInputs(phone, name, email) && !(phoneBook.has(phone))) {
+        phoneBook.set(phone, [name, email]);
 
+        return true;
+    }
+
+    return false;
+}
+
+function areCorrectInputs(phone, name, email) {
+    const phoneValid = /^[0-9]{10}$/.test(phone);
+    const nameValid = typeof (name) === 'string' && name !== '';
+    const emailValid = typeof (email) === 'string';
+
+    return phoneValid && nameValid && emailValid;
 }
 
 /**
@@ -30,7 +47,21 @@ function add(phone, name, email) {
  * @returns {Boolean}
  */
 function update(phone, name, email) {
+    if (typeof(email) === 'undefined') {
+        email = '';
+    }
+    if (!areCorrectInputs(phone, name, email)) {
+        return false;
+    }
+    for (var key of phoneBook.keys()) {
+        if (key === phone) {
+            phoneBook.set(phone, [name, email]);
 
+            return true;
+        }
+    }
+
+    return false;
 }
 
 /**
@@ -39,7 +70,29 @@ function update(phone, name, email) {
  * @returns {Number}
  */
 function findAndRemove(query) {
+    if (typeof (query) !== 'string') {
+        return false;
+    }
+    var convertedMap = сonvertMapToArray();
+    var keys = getKeys(phoneBook);
+    var removedCount = 0;
+    for (let i = 0; i < convertedMap.length; i++) {
+        if (convertedMap[i].indexOf(query) || query === '*') {
+            phoneBook.delete(keys[i]);
+            removedCount++;
+        }
+    }
 
+    return removedCount - 1;
+}
+
+function getKeys(keysArray) {
+    var keyArray = [];
+    for (let key of keysArray.keys()) {
+        keyArray.push(key);
+    }
+
+    return keyArray;
 }
 
 /**
@@ -48,7 +101,41 @@ function findAndRemove(query) {
  * @returns {String[]}
  */
 function find(query) {
+    if (typeof (query) !== 'string') {
+        return false;
+    }
+    var convertedMap = сonvertMapToArray();
+    var result = [];
+    for (let i = 0; i < convertedMap.length; i++) {
+        if (convertedMap[i].indexOf(query) || query === '*') {
+            result.push(convertedMap[i]);
+        }
+    }
 
+    return result.sort();
+}
+
+function сonvertMapToArray() {
+    let result = [];
+    for (var [key, value] of phoneBook) {
+        var truString = transformString(key, value[0], value[1]);
+        result.push(truString);
+    }
+
+    return result;
+}
+
+function transformString(phone, name, email) {
+    if (email.length === 0) {
+        email = '';
+    } else {
+        email = ', ' + email;
+    }
+    let changedPhone = '+7 (' + phone.slice(0, 3) + ') ' + phone.slice(3, 6) +
+        '-' + phone.slice(6, 8) + '-' + phone.slice(8, 10);
+    var changedName = name + ', ';
+
+    return changedName + changedPhone + email;
 }
 
 /**
